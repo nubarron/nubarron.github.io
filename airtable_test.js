2b801e3be21468d433790986dc2421d581c7e47a
nubarron <53066505+nubarron@users.noreply.github.com>
2023-06-24 11:36:37 -0400

    Update airtable_test.js

--- airtable_test.js
+++ airtable_test.js
@@ -1,4 +1,4 @@
-var Airtable = require('airtable');
+var Airtable = require('https://github.com/Airtable/airtable.js');
 Airtable.configure({
     endpointUrl: 'https://api.airtable.com',
     apiKey: 'patHXMh9QL5KMwJzG.d5cc1651970c910d9faab7bd5246c025f994540188a71e4c9dad38d6c1192f94'
