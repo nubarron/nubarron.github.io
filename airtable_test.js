1af1eefe3f42633abaff249e280634bb165ab668
nubarron <53066505+nubarron@users.noreply.github.com>
2023-06-24 11:34:26 -0400

    Create airtable_test.js

---
+++ airtable_test.js
@@ -0,0 +1,6 @@
+var Airtable = require('airtable');
+Airtable.configure({
+    endpointUrl: 'https://api.airtable.com',
+    apiKey: 'patHXMh9QL5KMwJzG.d5cc1651970c910d9faab7bd5246c025f994540188a71e4c9dad38d6c1192f94'
+});
+var base = Airtable.base('appBW71DB555EyIBL');
